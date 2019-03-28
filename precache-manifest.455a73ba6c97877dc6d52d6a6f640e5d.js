0c7bd23e290212feb97c18e6482776464c83d174
adel <adel.mohamed855@gmail.com>
2019-03-28 20:31:31 +0200

    Updates

---
+++ precache-manifest.455a73ba6c97877dc6d52d6a6f640e5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coligo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a57c08e58d6f306c38a6",
+    "url": "/coligo/static/js/runtime~main.5ffaf806.js"
+  },
+  {
+    "revision": "921951621fb561f3ba38",
+    "url": "/coligo/static/js/main.4730cc2f.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/coligo/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "921951621fb561f3ba38",
+    "url": "/coligo/static/css/main.e7bb26a6.chunk.css"
+  },
+  {
+    "revision": "bd0dd4bdd15c309903c5ceb8da45cd11",
+    "url": "/coligo/index.html"
+  }
+];
