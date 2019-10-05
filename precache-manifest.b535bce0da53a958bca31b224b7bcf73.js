1e596f0901d931136ed1af6358c4241f7a6c9c52
adel <>
2019-10-05 22:30:53 +0200

    Updates

---
+++ precache-manifest.b535bce0da53a958bca31b224b7bcf73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b6ab9230f77702c72c818169bd0c156",
+    "url": "/coligo/index.html"
+  },
+  {
+    "revision": "aaff7a6a4fe4e5cf1af5",
+    "url": "/coligo/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "f40558c59f1e1947be7a",
+    "url": "/coligo/static/css/main.74e9026a.chunk.css"
+  },
+  {
+    "revision": "aaff7a6a4fe4e5cf1af5",
+    "url": "/coligo/static/js/2.65ee616e.chunk.js"
+  },
+  {
+    "revision": "f40558c59f1e1947be7a",
+    "url": "/coligo/static/js/main.ba53e7fb.chunk.js"
+  },
+  {
+    "revision": "a57c08e58d6f306c38a6",
+    "url": "/coligo/static/js/runtime~main.5ffaf806.js"
+  },
+  {
+    "revision": "cf1b3e8a86dac06fe066071d53e6a56c",
+    "url": "/coligo/static/media/desk.cf1b3e8a.svg"
+  }
+]);
